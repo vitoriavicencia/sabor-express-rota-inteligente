c11f15b9a90464edcf3fea2148894ffbac540fa0
Vitória Vicência <vitoriavicenciasilva@gmail.com>
2026-09-13 01:42:07 +0000

    Adiciona funcao heuristica para uso no A-estrela

--- src/grafo.py
+++ src/grafo.py
@@ -1,5 +1,6 @@
 from collections import deque
-
+import heapq
+import math
 
 bairros = {"Loja": (0,0),
            "Centro": (2,1),
@@ -8,17 +9,15 @@ bairros = {"Loja": (0,0),
            "Bela_Vista": (2,5),
            "Parque_Alto": (2,4),}
 
-
-
-
 ruas = {
     "Loja":        [("Centro", 2.5)],
     "Centro":      [("Loja", 2.5), ("Jardim_Sul", 3.6), ("Parque_Alto", 3.2)],
     "Vila_Nova":   [("Jardim_Sul", 4.1), ("Bela_Vista", 5.0)],
     "Jardim_Sul":  [("Centro", 3.6), ("Vila_Nova", 4.1)],
-     "Bela_Vista":  [("Parque_Alto", 1.0), ("Vila_Nova", 5.0)],
+    "Bela_Vista":  [("Parque_Alto", 1.0), ("Vila_Nova", 5.0)],
     "Parque_Alto": [("Centro", 3.2), ("Bela_Vista", 1.0)],
 }
+
 
 def bfs(grafo, inicio, fim):
     visitados = {inicio}
@@ -57,9 +56,18 @@ def dfs(grafo, atual, fim, visitados=None, caminho=None):
     return None
 
 
+def heuristica(bairro_a, bairro_b):
+    x1, y1 = bairros[bairro_a]
+    x2, y2 = bairros[bairro_b]
+    return math.hypot(x2 - x1, y2 - y1)
+
+
 if __name__ == "__main__":
     caminho = bfs(ruas, "Loja", "Bela_Vista")
     print("Caminho encontrado pelo BFS:", caminho)
 
     caminho_dfs = dfs(ruas, "Loja", "Bela_Vista")
     print("Caminho encontrado pelo DFS:", caminho_dfs)
+
+    dist = heuristica("Loja", "Centro")
+    print("Distância em linha reta Loja -> Centro:", dist)
